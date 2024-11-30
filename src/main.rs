ae9a3306a73166c173f3de52a72df5b37e83f419
andi-mache <amache75@gmail.com>
2024-11-30 12:28:34 +0300

    feat: improve ui

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,206 @@
-// gui libraries
-use iced::{Application, Command};
-use iced::{
-    alignment::{Horizontal, Vertical},
-    widget::{column, container, Container},
-    Length, Settings,
-};
 
-// libs for chat funtionality
-use futures::stream::StreamExt;
-use libp2p::{gossipsub, mdns, noise, swarm::NetworkBehaviour, swarm::SwarmEvent, tcp, yamux};
-use std::collections::hash_map::DefaultHasher;
-use std::error::Error;
-use std::hash::{Hash, Hasher};
-use std::time::Duration;
-use tokio::{io, io::AsyncBufReadExt, select};
-use tracing_subscriber::EnvFilter;
+use iced::widget::{button, column,row, container, text,text_input, Column, Button, vertical_space, horizontal_space, vertical_rule, horizontal_rule};
+use iced::{Center, Element, Task, Theme, Fill};
 
-// structure for the gui
-struct LanChat;
+pub fn main() -> iced::Result {
+    iced::application("LAN-chat", App::update, App::view)
+        
+        .theme(App::theme)
+        .run_with(App::new)
 
-// structure for custom network behaviour that combines Gossipsub and Mdns.
-struct MyBehavior{
-    gossipsub: gossipsub::Behavior,
-    mdns: mdns::tokio::Behavior,
 }
 
-impl Application for LanChat {
-    type Executor = iced::executor::Default;
-    type Flags = ();
-    type Message = ();
+//#[derive(Default)]
+struct App {
+    screen: Screen,
+    value: i64,
 
-    fn new(_flags: ()) -> (LanChat, Command<Self::Message>) {
-        (LanChat, Command::none())
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    LoginButtonPressed,
+    BackPressed,
+    Increment,
+    Decrement,
+}
+
+impl App {
+    fn new() -> (Self, Task<Message>) {
+        ( Self {
+            screen: Screen::Login,
+            value: 0,
+            
+    
+        },
+        Task::none(),
+        )
     }
 
-    fn title(&self) -> String {
-        String::from(" andi's LAN Chat")
+
+    fn update(&mut self, message: Message) -> Task<Message>{
+        match message {
+            Message::BackPressed => {
+                if let Some(screen) = self.screen.previous() {
+                    self.screen = screen;
+                }
+
+
+                Task::none()
+            }
+            Message::LoginButtonPressed => {
+                               if let Some(screen) = self.screen.next() {
+                    self.screen = screen;
+                }
+
+                Task::none() 
+            }
+            Message::Increment => {
+                self.value += 1;
+
+                Task::none()
+
+            }
+            Message::Decrement => {
+            
+                self.value -= 1;
+
+                Task::none()
+            }
+
+        }
+    }
+    fn view(&self) -> Element<Message> {
+        let screen = match self.screen {
+            Screen::Login => self.login(),
+            Screen::Home => self.home(),
+            Screen::End => self.end(),
+            
+        };
+
+        let content: Element<_> = column![screen,]
+        .width(Fill)
+        .spacing(20)
+        .padding(20)
+        .into();
+
+        container(content).center(Fill).into()
+
+    }
+    fn can_continue(&self) -> bool {
+        match self.screen {
+            Screen::Login => true,
+            Screen::Home => true,
+            Screen::End => false,
+        }
     }
 
-    fn update(&mut self, _message: Self::Message) -> Command<Self::Message> {
-        Command::none()
+    fn end(&self) -> Column<Message> {
+        Self::container("You reached the end!")
+            .push("This tour will be updated as more features are added.")
+            .push("Make sure to keep an eye on it!")
     }
 
-    fn view(&mut self) -> iced::Element<'_, Self::Message> {
-        iced::Text::new("Hello, LAN Chat!").into()
+    fn theme(&self) -> Theme {
+                   Theme::Dark
+         }
+    fn login(&self) -> Column<Message> {
+
+        let user_input = text_input("username", "").width(400);
+        let pass_input = text_input("password", "").width(400);
+        let submit_button = button("submit").on_press(Message::LoginButtonPressed);
+        let register_button = button("register_button");
+        let input_controls = row![register_button,submit_button].padding(20).spacing(20);
+
+        column![
+            text("yoy").size(70),
+            user_input,
+            pass_input,
+            input_controls,
+            vertical_space(),
+            //padded_button("Next").on_press(Message::LoginButtonPressed),
+        ].width(Fill).align_x(Center).padding(20).spacing(20)        
+        
+    }
+
+    fn home(&self) -> Column<Message> {
+
+        let topcontrols = row![
+            button("one"),button("two")       ,     button("BackPressed").on_press(Message::BackPressed),
+        ].spacing(20);
+        let sidebar = column![button("sdsds"), button("sees")].spacing(20);
+        let chat_controls = row![
+            text_input("Nick","").width(75),
+            text_input("message",""),
+            button("Send"),
+    
+        ].spacing(5);
+        //let body
+        let content = column![
+            button("Increment").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::Decrement),
+            vertical_space(),
+            horizontal_rule(10),
+            chat_controls,
+        ].align_x(Center);
+        let main_body = row![sidebar, vertical_rule(10), content, vertical_rule(10)];
+        column![
+            topcontrols,
+            horizontal_rule(10),
+            main_body,
+            horizontal_rule(10),
+                   ]
+       
+    }
+
+        fn container(title: &str) -> Column<'_, Message> {
+        column![text(title).size(50)].spacing(20)
     }
 }
 
-fn render_ui() -> iced::Result{
-    LanChat::run(iced::Settings::default())
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Screen {
+    Login,
+    Home,
+    End,
 }
 
-#[tokio::main]
-async fn main() {
-    render_ui()
+impl Screen {
+    const ALL: &'static [Self] = &[
+        Self::Login,
+        Self::Home,
+        Self::End,
+    ];
+
+    pub fn next(self) -> Option<Screen> {
+        Self::ALL
+            .get(
+                Self::ALL
+                    .iter()
+                    .copied()
+                    .position(|screen| screen == self)
+                    .expect("Screen must exist")
+                    + 1,
+            )
+            .copied()
+    }
+
+    pub fn previous(self) -> Option<Screen> {
+        let position = Self::ALL
+            .iter()
+            .copied()
+            .position(|screen| screen == self)
+            .expect("Screen must exist");
+
+        if position > 0 {
+            Some(Self::ALL[position - 1])
+        } else {
+            None
+        }
+    }
+}
+
+fn padded_button<Message: Clone>(label: &str) -> Button<'_, Message> {
+    button(text(label)).padding([12, 24])
 }
